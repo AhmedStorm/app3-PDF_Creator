d81a82d4a23a0cc42f73feb87522036a52ecf63e
Barhoma <ahmedtbarhoma@gmail.com>
2023-07-22 15:39:32 +0300

    Add more Pages for each Topic

--- main.py
+++ main.py
@@ -9,8 +9,10 @@ for index, item in df.iterrows():
     asd.add_page()
     asd.set_font(family="Times", style="B", size=24)
     asd.set_text_color(100, 100, 100)
-    asd.cell(w=0, h=24, txt=item['Topic'], border=0, ln=1)
+    asd.cell(w=0, h=24, txt=item['Topic'], border=0, ln=1 , align="L")
     asd.line(x1=12, y1=27, x2=200, y2=27)
+    for i in range(item['Pages'] - 1):
+        asd.add_page()
 
 
 asd.output("Mine.pdf")
